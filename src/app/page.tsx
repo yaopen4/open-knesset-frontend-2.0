7f62adcb5b4d0b3d93d7969a4afcddf553cc7e25
Yehonatan Abramov <yaopen4@gmail.com>
2025-11-11 09:05:20 +0000

    in the main page, the banner in the bottom sticks to the right again, th

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -95,7 +95,7 @@ export default function AboutPage() {
           </p>
         </div>
       </div>
-      <footer className="fixed bottom-0 right-0 w-[calc(100%-var(--sidebar-width))] bg-primary py-2 text-center text-primary-foreground group-data-[collapsible=icon]/sidebar-wrapper:w-[calc(100%-var(--sidebar-width-icon))] md:group-data-[collapsible=offcanvas]/sidebar-wrapper:w-full">
+      <footer className="fixed bottom-0 left-0 w-[calc(100%-var(--sidebar-width))] bg-primary py-2 text-center text-primary-foreground group-data-[collapsible=icon]/sidebar-wrapper:w-[calc(100%-var(--sidebar-width-icon))] md:group-data-[collapsible=offcanvas]/sidebar-wrapper:w-full">
         <p>© הסדנא לידע ציבורי (ע"ר) 2025</p>
       </footer>
     </div>
